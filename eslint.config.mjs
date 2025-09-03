8392ab0ef9b61546677319d2e166f2bf64f45f5a
Sergio Anselmi <sergioanselmi@gmail.com>
2025-09-03 18:56:56 -0300

    Ejercicio en clase

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,6 @@ import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
   pluginReact.configs.flat.recommended,
 ]);
